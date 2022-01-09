00b1e54d46baa5087a4d9dc1e0befa593cccca85
Max Yuzhakov <maxyuzhakov@gmail.com>
2022-01-09 18:09:43 +0500

    7th commit: - Media serving functionality;

--- blogProject/urls.py
+++ blogProject/urls.py
@@ -17,14 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from users import views as user_views
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blogs.urls')),
-    path('', include('users.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#media(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+                  path('admin/', admin.site.urls),
+                  path('', include('blogs.urls')),
+                  path('', include('users.urls')),
+              ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
